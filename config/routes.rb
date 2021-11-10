4501e84df6b624e86c5dfdc2148740cb60603fbe
prolajumokeoni <prolajumokeoni@gmail.com>
2021-11-10 14:12:14 +0100

    Edit routes to send all requests

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: 'json' } do
     get 'greetings', to: 'greetings#index'
   end
+  get '*page', to: 'static#index', constraints: -> (req) do
+    !req.xhr? && req.format.html?
+  end
 end
