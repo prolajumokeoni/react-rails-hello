8a04b72e2cac083a799563c4d95633414c5b760e
prolajumokeoni <prolajumokeoni@gmail.com>
2021-11-11 08:02:37 +0100

    Setup sample data in  postgress database

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,16 +1,17 @@
-import React from "react"
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Greeting from './Greeting'
-class App extends React.Component {
-  render() {
-    return (
-        <BrowserRouter>
-          <Routes>
-          <Route path="/" exact element={<Greeting />} />
-          </Routes>
-        </BrowserRouter>
-    );
-  }
-}
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Greeting from './Greeting';
+import store from '../redux/store';
 
-export default App
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
+
+export default App;
